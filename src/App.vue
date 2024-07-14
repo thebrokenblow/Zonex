242c14d42cea9ba8d16cb2060d6be7c2f941e16b
Artem <artkrasov@gmail.com>
2024-07-14 23:26:43 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeaderComponent from './components/HeaderComponent.vue'
+</script>
+
+<template>
+  <div>
+    <HeaderComponent />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <nav class="nav header__nav">
+            <ul class="nav__list">
+                <li class="nav_item"><a href="" class="nav__link">Home</a></li>
+                <li class="nav_item"><a href="" class="nav__link">Shop</a></li>
+                <li class="nav_item"><a href="" class="nav__link">Elements</a></li>
+                <li class="nav_item"><a href="" class="nav__link">Blog</a></li>
+                <li class="nav_item"><a href="" class="nav__link">Page</a></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
